1b2d67a49405b07486b62d61872f355fac76fa12
lucasflaquer <lucas.flaquer@gmail.com>
2019-02-23 13:17:12 -0300

    style corrections and functionality added

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,19 @@
-const menu = document.querySelectorAll('.side-menu--list');
-let i;
+const list = document.querySelectorAll('.side-menu--list');
+$(document).ready(function () {
+	change();
+});
+
+
+function change() {
+	let item = $('.side-menu--item a');
+	item.on('click', function() {
+		let href = $(this).attr('href');
+		console.log(href);
+		console.log($(this));
+
+		// $(href).toggleClass('active');
+		// console.log($(href));
+		// let target = href.replace('#', '');
+		// console.log(target);
+	});
+}
